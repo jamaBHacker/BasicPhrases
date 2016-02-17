bbcc451d80ad8ec119f77e3cfaeec1cf2b7b38a4
Jamal Butcher <jamalbutcher@gmail.com>
2016-02-17 16:27:04 -0400

    first commit

---
+++ app/src/main/java/com/jamalbutcher/basicphrases/MainActivity.java
@@ -0,0 +1,56 @@
+package com.jamalbutcher.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view) {
+        int id = view.getId();
+
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        Log.i("OurId", ourId);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.jamalbutcher.basicphrases");
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+
+        mediaPlayer.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
